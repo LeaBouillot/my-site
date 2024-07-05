22f50995b932fb4e6bc7df269beb834d2929af9c
bestdevelope <leabouillot7@gmail.com>
2024-07-05 15:08:56 +0200

    nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
-// import { useState } from 'react'
+import Home from './pages/Home.tsx';
+import About from './pages/About.tsx';
+import Resume from './pages/Resume.tsx';
+import Portfolio from './pages/Portfolio.tsx';
+import Services from './pages/Services.tsx';
+import Contact from './pages/Contact.tsx';
+import { BrowserRouter as Router, Route, Routes, Navigate} from'react-router-dom';
+import "./App.css"
 
-import './App.css'
+export default function App() {
 
-function App() {
-  // const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
-  )
+<Router>
+  <div>
+    {/* <Routes>
+      <Route path="/" element={<Navbar />} />
+      <Route path="*" element={() => <Navigate to="/" replace />} />
+    </Routes> */}
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/resume" element={<Resume />} />
+      <Route path="/portfolio" element={<Portfolio />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  </div>
+</Router>
+  );
 }
-
-export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,40 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-import Home from './pages/Home.tsx';
-import About from './pages/About.tsx';
-import Resume from './pages/Resume.tsx';
-import Portfolio from './pages/Portfolio.tsx';
-import Services from './pages/Services.tsx';
-import Contact from './pages/Contact.tsx';
-import { createBrowserRouter, RouterProvider } from'react-router-dom';
+import React from "react";
+import ReactDom from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import "./output.css";
 
-const router = createBrowserRouter([
-  { path: "/",
-    element: <Home />,
-  },
-  { path: "/about",
-    element: <About />,
-  },
-  { path: "/resume",
-    element: <Resume />,
-  },
-  { path: "/portfolio",
-    element: <Portfolio />,
-  },
-  { path: "/services",
-    element: <Services />,
-  },
-  { path: "/contact",
-    element: <Contact />,
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDom.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router}>
       <App />
-    </RouterProvider>
   </React.StrictMode>,
 );
+
