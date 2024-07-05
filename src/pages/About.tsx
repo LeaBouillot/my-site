1fc3f03fc944b037397ae94428b71f40a5425e82
bestdevelope <leabouillot7@gmail.com>
2024-07-05 16:30:08 +0200

    icon

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,10 +4,12 @@ export default function About() {
 
     return (
         <main>
+            <div> 
             <Navbar />
-            <p>
-            Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.
-            </p>
+            <h2>About</h2>
+          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+        </div>
+          
             <h2>
             UI/UX Designer & Web Developer.
             </h2>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,41 @@
 import Navbar from "../components/Navbar";
+import Icons from "../components/Icons";
+import backgroundImage from "../assets/hero-bg.jpg";
 
 export default function Home() {
-    return (
-        <main>
-            <Navbar />
-            <h1>Léa Bouillot</h1>
-            <p>I'm Developpeur</p>
-        </main>
-    )
+  const homeStyle = {
+    position: "relative",
+    height: "100vh",
+    color: "white",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
+    overflow: "hidden",
+  };
+
+  const backgroundStyle = {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    objectFit: "cover",
+    zIndex: -1,
+  };
+
+  return (
+    <main style={homeStyle}>
+      <img
+        src={backgroundImage}
+        alt="background image"
+        style={backgroundStyle}
+      />
+      <Navbar />
+      <h1>Brandon Johnson</h1>
+      <p>I'm Developpeur</p>
+      <Icons />
+    </main>
+  );
 }
