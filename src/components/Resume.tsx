5585c73cb768467c3d9715375cbca1188ed241fb
bestdevelope <leabouillot7@gmail.com>
2024-07-08 00:54:02 +0200

    resume

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -1,16 +1,28 @@
-import Navbar from "./Header";
+import React from "react";
+
+import { resumeText } from "../constants";
 
 export default function Resume() {
   return (
     <main>
-      <Navbar />
-      <h1>Résumé</h1>
+      <h2>Résumé</h2>
       <p>
         Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex
         aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos
         quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat
         sit in iste officiis commodi quidem hic quas.
       </p>
+      <div className="resume__desc">
+        {resumeText.map((resume, key) => (
+          <div key={key}>
+            <span>{key + 1}.</span>
+            <h3>{resume.title}</h3>
+            <p>{resume.year}</p>
+            <p>{resume.subtitle}</p>
+            <p>{resume.desc}</p>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
