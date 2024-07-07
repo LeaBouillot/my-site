767767d9b0785ad87c6785d860fe8c1b46ebb080
bestdevelope <leabouillot7@gmail.com>
2024-07-08 00:04:53 +0200

    Home-icons

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,58 +1,23 @@
-import Navbar from "./Header";
-import Icons from "./Icons";
+import React from "react";
+
+import { icon } from "../constants";
+
 import backgroundImage from "../assets/hero-bg.jpg";
 
 export default function Home() {
-  const homeStyle = {
-    position: "relative",
-    height: "100vh",
-    color: "white",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    textAlign: "center",
-    overflow: "hidden",
-  };
-
-  const backgroundStyle = {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    width: "100%",
-    height: "100%",
-    objectFit: "cover",
-    zIndex: -1,
-  };
-
-  const overlayStyle = {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    width: "100%",
-    height: "100%",
-    backgroundColor: "rgba(0, 0, 0, 0.5)", // Couleur de l'overlay avec transparence
-    zIndex: 0,
-  };
-
-  const contentStyle = {
-    position: "relative",
-    zIndex: 1,
-  };
-
   return (
-    <main style={homeStyle}>
-      <img
-        src={backgroundImage}
-        alt="background image"
-        style={backgroundStyle}
-      />
-      <div style={overlayStyle}></div>
-      <div style={contentStyle}>
-        <Navbar />
-        <h1>Brandon Johnson</h1>
-        <p>I'm Developpeur</p>
-        <Icons />
+    <main>
+      <img src={backgroundImage} alt="background image" />
+      <div>
+        <ul>
+          {icon.map((icon, key) => (
+            <li key={key}>
+              <a href={icon.url}>
+                <img src={icon.icon} alt={icon.title} />
+              </a>
+            </li>
+          ))}
+        </ul>
       </div>
     </main>
   );
