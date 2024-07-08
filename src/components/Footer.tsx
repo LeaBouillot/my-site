ad50d7dcf0cb076e88ed70b6b8af00593cefbc2f
bestdevelope <leabouillot7@gmail.com>
2024-07-08 09:20:19 +0200

    icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,23 +1,22 @@
 import React from "react";
 
-import {icon} from "../constants"
+import { icons } from "../constants";
 
 export default function Footer() {
-
-    return (
-        <footer>
-           <div className="">
-            <h3>Brandon Johnson</h3>
-            <ul>
-              {icon.map((footer, key) => (
-                <li key={key}>
-                  <a href={footer.link} target="_blank" rel="noreferrer">
-                    <img src={footer.icon} alt={footer.title} />
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </footer>
-    )
+  return (
+    <footer>
+      <div className="">
+        <h3>Brandon Johnson</h3>
+        <ul>
+          {icons.map((footer, key) => (
+            <li key={key}>
+              <a href={footer.link} target="_blank" rel="noreferrer">
+                <img src={footer.icons} alt={footer.title} />
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </footer>
+  );
 }
