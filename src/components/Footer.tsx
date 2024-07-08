a5d31d4e4489b0387cc1cfed080b1b85d7bc18b1
bestdevelope <leabouillot7@gmail.com>
2024-07-08 09:17:56 +0200

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 
+import {icon} from "../constants"
+
 export default function Footer() {
 
     return (
         <footer>
-            <p>�� 2023 Léa Bouillot. All rights reserved.</p>
+           <div className="">
+            <h3>Brandon Johnson</h3>
+            <ul>
+              {icon.map((footer, key) => (
+                <li key={key}>
+                  <a href={footer.link} target="_blank" rel="noreferrer">
+                    <img src={footer.icon} alt={footer.title} />
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
         </footer>
     )
 }
