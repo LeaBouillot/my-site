ac6d04c79b8a62c338da1a082f98ca60caa5fe7e
bestdevelope <leabouillot7@gmail.com>
2024-07-13 17:50:35 +0200

    router

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 import ReactDom from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "aos/dist/aos.css";
+import AOS from "aos";
 import App from "./App.tsx";
+import Portfolio_details from "./components/Portfolio_details.tsx";
+
+AOS.init();
+
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/portfolio-details", element: <Portfolio_details /> },
+]);
 
 ReactDom.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
